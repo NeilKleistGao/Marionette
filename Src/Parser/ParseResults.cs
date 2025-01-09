7ccc1ae0c9271e296a22684a9f23e0f567ec635b
NeilKleistGao <neilkleist@gmail.com>
2025-01-09 11:03:18 +0800

    WIP: Add parser structure

--- Src/Parser/ParseResults.cs
+++ Src/Parser/ParseResults.cs
@@ -19,19 +19,31 @@ namespace Marionette.Parser {
     }
   }
 
-  public abstract class Literal<T, TRes, TEnv>: IEvaluatable<TRes, TEnv> {
-    protected T Value;
-
+  public abstract class Literal<TRes, TEnv>: IEvaluatable<TRes, TEnv> {
     public TRes Evaluate(TEnv env) {
       throw new NotImplementedException();
     }
   }
+
+  public class IntLiteral<TRes, TEnv> : Literal<TRes, TEnv> {
+    public int Value { get; set; }
+  }
+
+  public class NumberLiteral<TRes, TEnv> : Literal<TRes, TEnv> {
+    public double Value { get; set; }
+  }
+
+  // TODO: more literals
 
   public abstract class Symbol<TRes, TEnv>: IEvaluatable<TRes, TEnv> {
     private string name;
 
     public TRes Evaluate(TEnv env) {
       throw new NotImplementedException();
+    }
+
+    public Symbol(string name) {
+      this.name = name;
     }
   }
 } // namespace Marionette.Parser
--- Src/Parser/Parser.cs
+++ Src/Parser/Parser.cs
@@ -1,11 +1,55 @@
+using System;
+using System.Collections;
+
 namespace Marionette.Parser {
-  public class Parser {
+  public class Parser<TRes, TEnv> : IEnumerator<IEvaluatable<TRes, TEnv>> {
     private string code;
     private Utils.Position position;
+    private int pointer = -1;
 
-    public Parser(string code) {
+    public delegate ResultList<TRes, TEnv> ListAllocator(IEvaluatable<TRes, TEnv>[] subterms);
+
+    public delegate Literal<TRes, TEnv> LiteralAllocator(string text);
+
+    public delegate Symbol<TRes, TEnv> SymbolAllocator(string name);
+
+    private ListAllocator allocateList;
+    private LiteralAllocator allocateLiteral;
+    private SymbolAllocator allocateSymbol;
+
+    private IEvaluatable<TRes, TEnv>? curResult = null;
+
+    public Parser(string code, ListAllocator allocateList, LiteralAllocator allocateLiteral, SymbolAllocator allocateSymbol) {
       this.code = code;
-      this.position = new Utils.Position(0, 1);
+      this.allocateList = allocateList;
+      this.allocateLiteral = allocateLiteral;
+      this.allocateSymbol = allocateSymbol;
+      position = new Utils.Position(0, 1);
+    }
+
+    private IEvaluatable<TRes, TEnv> Parse() {
+      throw new NotImplementedException();
+    }
+
+    public IEvaluatable<TRes, TEnv> Current => curResult ?? (curResult = Parse());
+
+    object IEnumerator.Current => curResult ?? (curResult = Parse());
+
+    public void Dispose() {}
+
+    public bool MoveNext() {
+      if (pointer >= code.Length) {
+        return false;
+      }
+      else {
+        curResult = Parse();
+        return true;
+      }
+    }
+
+    public void Reset() {
+      position = new Utils.Position(0, 1);
+      pointer = -1;
     }
   }
 } // namespace Marionette.Parser
